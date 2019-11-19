6648be0075193b23608a6c24359ef2aaf7d55c33
Tarsupon <alexeyka981@gmail.com>
2019-11-19 21:54:43 +0300

    add test in core module

--- postsapp/src/app/core/posts.service.spec.ts
+++ postsapp/src/app/core/posts.service.spec.ts
@@ -1,12 +1,43 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { PostsService } from './posts.service';
+import { Post } from '../shared/models/post';
 
-describe('PostsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+describe('PostsService -- testing http get request', () => {
+  let httpTestingController: HttpTestingController;
 
-  it('should be created', () => {
-    const service: PostsService = TestBed.get(PostsService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [PostsService]
+    });
+
+    httpTestingController = TestBed.get(HttpTestingController);
+
   });
+
+  it('can test HttpClient.get', () => {
+    const data = {
+      userId: 1,
+      id: 1,
+      title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
+      body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto'
+    };
+
+    const service: PostsService = TestBed.get(PostsService);
+
+    service.getPosts().pipe().subscribe((response: Post[]) => {
+
+      expect(response[0].body).toBe(data.body);
+    });
+
+    const req = httpTestingController.expectOne('https://jsonplaceholder.typicode.com/posts');
+
+    expect(req.request.method).toBe('GET');
+
+    req.flush(data);
+  });
+
+  afterEach(() => httpTestingController.verify());
+
 });
